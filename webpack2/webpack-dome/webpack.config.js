8cb55cc748cc6567866df73fc209081554ae05e9
Lemon <1217032700@qq.com>
2021-08-05 17:13:03 +0800

    测试a标签跳转

--- webpack2/webpack-dome/webpack.config.js
+++ webpack2/webpack-dome/webpack.config.js
@@ -1,13 +1,29 @@
 const path = require('path');
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
+  // entry: './src/index.js',
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // title: '管理输出',
+      title: 'Development',
+    }),
+  ],
   output: {
     // filename: 'main.js',
-    filename: 'bundle.js',
+    // filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
@@ -23,35 +39,35 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-      {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
-        },
-      },
+      // {
+      //   test: /\.(csv|tsv)$/i,
+      //   use: ['csv-loader'],
+      // },
+      // {
+      //   test: /\.xml$/i,
+      //   use: ['xml-loader'],
+      // },
+      // {
+      //   test: /\.toml$/i,
+      //   type: 'json',
+      //   parser: {
+      //     parse: toml.parse,
+      //   },
+      // },
+      // {
+      //   test: /\.yaml$/i,
+      //   type: 'json',
+      //   parser: {
+      //     parse: yaml.parse,
+      //   },
+      // },
+      // {
+      //   test: /\.json5$/i,
+      //   type: 'json',
+      //   parser: {
+      //     parse: json5.parse,
+      //   },
+      // },
     ],
   },
 };
